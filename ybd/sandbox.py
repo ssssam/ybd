744c72704ce91878f8bc2a40dac45ea228a32d97
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2016-03-04 18:22:34 +0900

    sandbox.py: Experimental aboriginal support

--- ybd/sandbox.py
+++ ybd/sandbox.py
@@ -14,15 +14,13 @@
 #
 # =*= License: GPL-2 =*=
 
-
-import sandboxlib
 import contextlib
 import os
 import pipes
 import shutil
 import stat
 import tempfile
-from subprocess import call, PIPE
+from subprocess import call, check_call, PIPE
 
 import app
 import cache
@@ -30,9 +28,25 @@ import utils
 from repos import get_repo_url
 
 
-# This must be set to a sandboxlib backend before the run_sandboxed() function
-# can be used.
-executor = None
+def _aboriginal_start():
+    # Assert configuration
+    #
+    if 'aboriginal-controller' not in app.config:
+        app.exit(this, 'ERROR: required configuration not specified:', 'aboriginal-controller')
+    if 'aboriginal-system' not in app.config:
+        app.exit(this, 'ERROR: required configuration not specified:', 'aboriginal-system')
+
+    #
+    # Ensure we have a worker for this instance
+    #
+    workdir = os.path.join(app.config['workers'], ('worker-' + str(app.config.get('fork', 0))))
+    aboriginal_start = os.path.join(app.config['aboriginal-controller'], 'aboriginal-start')
+    check_call([ aboriginal_start,
+                 '--silent',
+                 '--cpus', str(app.config['max-jobs']),
+                 '--emulator', app.config['aboriginal-system'],
+                 '--workdir', workdir,
+                 '--directory', app.config['tmp'] ])
 
 
 @contextlib.contextmanager
@@ -55,6 +69,10 @@ def setup(this):
     assembly_dir = this['sandbox']
     for directory in ['dev', 'tmp']:
         call(['mkdir', '-p', os.path.join(assembly_dir, directory)])
+
+    # Start up the aboriginal worker for this instance, this should
+    # probably be done elsewhere but works here
+    _aboriginal_start()
 
     try:
         yield
@@ -81,11 +99,7 @@ def install(defs, this, component):
 def ldconfig(this):
     conf = os.path.join(this['sandbox'], 'etc', 'ld.so.conf')
     if os.path.exists(conf):
-        path = os.environ['PATH']
-        os.environ['PATH'] = '%s:/sbin:/usr/sbin:/usr/local/sbin' % path
-        cmd_list = ['ldconfig', '-r', this['sandbox']]
-        run_logged(this, cmd_list)
-        os.environ['PATH'] = path
+        run_sandboxed (this, 'ldconfig')
     else:
         app.log(this, 'No %s, not running ldconfig' % conf)
 
@@ -95,95 +109,99 @@ def argv_to_string(argv):
 
 
 def run_sandboxed(this, command, env=None, allow_parallel=False):
-    global executor
 
-    app.log(this, 'Running command:\n%s' % command)
-    with open(this['log'], "a") as logfile:
-        logfile.write("# # %s\n" % command)
-
-    mounts = ccache_mounts(this, ccache_target=env['CCACHE_DIR'])
-
-    if this.get('build-mode') == 'bootstrap':
-        # bootstrap mode: builds have some access to the host system, so they
-        # can use the compilers etc.
-        tmpdir = app.config.get("TMPDIR", "/tmp")
-
-        writable_paths = [this['build'], this['install'], tmpdir, ]
-
-        config = dict(
-            cwd=this['build'],
-            filesystem_root='/',
-            filesystem_writable_paths=writable_paths,
-            mounts='isolated',
-            extra_mounts=[],
-            network='isolated',
-        )
-    else:
-        # normal mode: builds run in a chroot with only their dependencies
-        # present.
-
-        mounts.extend([('tmpfs', '/dev/shm', 'tmpfs'),
-                       ('proc', '/proc', 'proc'), ])
-
-        if this.get('kind') == 'system':
-            writable_paths = 'all'
-        else:
-            writable_paths = [
-                this['name'] + '.build',
-                this['name'] + '.inst',
-                '/dev', '/proc', '/tmp',
-            ]
-
-        config = dict(
-            cwd=this['name'] + '.build',
-            filesystem_root=this['sandbox'],
-            filesystem_writable_paths=writable_paths,
-            mounts='isolated',
-            extra_mounts=mounts,
-            network='isolated',
-        )
-
-    argv = ['sh', '-c', command]
-
-    cur_makeflags = env.get("MAKEFLAGS")
-
-    # Adjust config for what the backend is capable of. The user will be warned
-    # about any changes made.
-    config = executor.degrade_config_for_capabilities(config, warn=False)
-
-    try:
+    cur_makeflags=None
+    if env is not None:
+        cur_makeflags = env.get("MAKEFLAGS")
         if not allow_parallel:
             env.pop("MAKEFLAGS", None)
 
-        app.log_env(this['log'], env, argv_to_string(argv))
-
-        with open(this['log'], "a") as logfile:
-            exit_code = executor.run_sandbox_with_redirection(
-                argv, stdout=logfile, stderr=sandboxlib.STDOUT,
-                env=env, **config)
-
-        if exit_code != 0:
-            app.log(this, 'ERROR: command failed in directory %s:\n\n' %
-                    os.getcwd(), argv_to_string(argv))
-            call(['tail', '-n', '200', this['log']])
-            app.log(this, 'ERROR: log file is at', this['log'])
-            app.exit(this, 'ERROR: sandbox debris is at', this['sandbox'])
-    finally:
-        if cur_makeflags is not None:
-            env['MAKEFLAGS'] = cur_makeflags
-
-
-def run_logged(this, cmd_list):
-    app.log_env(this['log'], os.environ, argv_to_string(cmd_list))
+        app.log(this, 'Running command:\n%s' % command)
     with open(this['log'], "a") as logfile:
-        if call(cmd_list, stdin=PIPE, stdout=logfile, stderr=logfile):
-            app.log(this, 'ERROR: command failed in directory %s:\n\n' %
-                    os.getcwd(), argv_to_string(cmd_list))
-            call(['tail', '-n', '200', this['log']])
-            app.exit(this, 'ERROR: log file is at', this['log'])
+        logfile.write("# # %s\n" % command)
+
+    # First serialize the build command into a ./build.sh script in the root
+    # of the staging area at this['sandbox']
+    #
+    # Script should start with cd /this['name'].build
+    aboriginal_control = os.path.join(app.config['aboriginal-controller'], 'aboriginal-controller')
+    workdir = os.path.join(app.config['workers'], ('worker-' + str(app.config.get('fork', 0))))
+    sandboxdir = os.path.basename(this['sandbox'].rstrip(os.path.sep))
+    builddir = this['name'] + '.build';
+    buildscript = os.path.join(this['sandbox'], 'build.sh')
+    buildlog = os.path.join(this['sandbox'], 'build.log')
+
+    with open(buildscript, "w") as script:
+        script.write ('#!/bin/bash\n')
+        script.write ('\n')
+
+        if env is not None:
+            script.write ('# Setup environment\n')
+            for envvar, envval in env.iteritems():
+                script.write ('export %s="%s"\n' % (envvar, envval))
+            script.write ('\n')
+
+        script.write ('# Switch to build directory\n')
+        script.write ('cd %s\n' % builddir)
+        script.write ('\n')
+
+        script.write ('# Run commands\n')
+        script.write ('%s\n' % command)
+
+    st = os.stat(buildscript)
+    os.chmod(buildscript, st.st_mode | stat.S_IEXEC)
+
+    # Prepare arguments to launch the aboriginal build
+    #
+    launchArgs = [ aboriginal_control,
+                   '--workdir', workdir,
+                   '--directory', sandboxdir,
+                   '--execute' ]
+
+    if this.get('build-mode') == 'prelibc':
+        launchArgs.append('--bootstrap')
+
+    if app.config.get('log-verbose'):
+        launchArgs.append('--verbose')
+
+    launchArgs.append('./build.sh > build.log 2>&1')
+
+    # Tail the build log into the expected location while the build runs
+    exit_code = call(launchArgs)
+
+    # Append this build log to the main log file, it's possible
+    try:
+        with open(this['log'], 'a') as logfile, open(buildlog, 'r') as commandlog:
+            logfile.write(commandlog.read())
+    except:
+        # We ran build.sh which asks for #!/bin/bash and directed to build.log,
+        # even if there is no output this will create build.log, the only way
+        # to reach this exception is if /bin/bash failed to run, this can happen.
+        app.log(this, 'ERROR: Non functional shell while running command:\n\n', command)
+        call(['tail', '-n', '200', this['log']])
+        app.log(this, 'ERROR: log file is at', this['log'])
+        app.exit(this, 'ERROR: sandbox debris is at', this['sandbox'])
+
+    if exit_code != 0:
+        app.log(this, 'ERROR: command failed in directory %s:\n\n' %
+                os.getcwd(), command)
+        call(['tail', '-n', '200', this['log']])
+        app.log(this, 'ERROR: log file is at', this['log'])
+        app.exit(this, 'ERROR: sandbox debris is at', this['sandbox'])
+
+    if cur_makeflags is not None:
+        env['MAKEFLAGS'] = cur_makeflags
+
+    return exit_code
 
 
 def run_extension(this, deployment, step, method):
+
+    # XXX This needs to be fixed to run inside the sandbox,
+    #     we need to first ensure we have built and staged all the
+    #     host tooling, i.e. python, in the image in order to
+    #     run the extensions, then we need to actually run the
+    #     extensions in the sandbox.
     app.log(this, 'Running %s extension:' % step, method)
     extensions = utils.find_extensions()
     tempfile.tempdir = tmp = app.config['tmp']
@@ -225,24 +243,8 @@ def run_extension(this, deployment, step, method):
             os.remove(cmd_tmp.name)
     return
 
-
-def ccache_mounts(this, ccache_target):
-    if app.config['no-ccache'] or 'repo' not in this:
-        mounts = []
-    else:
-        name = os.path.basename(get_repo_url(this['repo']))
-
-        ccache_dir = os.path.join(app.config['ccache_dir'], name)
-        if not os.path.isdir(ccache_dir):
-            os.mkdir(ccache_dir)
-
-        mounts = [(ccache_dir, ccache_target, None, 'bind')]
-    return mounts
-
-
 def env_vars_for_build(defs, this):
     env = {}
-    extra_path = []
     arch_dict = {
         'i686': "x86_32",
         'armv8l64': "aarch64",
@@ -253,45 +255,12 @@ def env_vars_for_build(defs, this):
         'mips32l': 'mipsel',
     }
 
-    if app.config['no-ccache']:
-        ccache_path = []
-    else:
-        ccache_path = ['/usr/lib/ccache']
-        env['CCACHE_DIR'] = '/tmp/ccache'
-        env['CCACHE_EXTRAFILES'] = ':'.join(
-            f for f in ('/baserock/binutils.meta',
-                        '/baserock/eglibc.meta',
-                        '/baserock/gcc.meta') if os.path.exists(f))
-        if not app.config.get('no-distcc'):
-            env['CCACHE_PREFIX'] = 'distcc'
-
-    prefixes = []
-
-    for name in this.get('build-depends', []):
-        dependency = defs.get(name)
-        prefixes.append(dependency.get('prefix', '/usr'))
-    prefixes = set(prefixes)
-    for prefix in prefixes:
-        if prefix:
-            bin_path = os.path.join(prefix, 'bin')
-            extra_path += [bin_path]
-
-    if this.get('build-mode') == 'bootstrap':
-        rel_path = extra_path + ccache_path
-        full_path = [os.path.normpath(this['sandbox'] + p) for p in rel_path]
-        path = full_path + app.config['base-path']
-        env['DESTDIR'] = this.get('install')
-    else:
-        path = extra_path + ccache_path + app.config['base-path']
-        env['DESTDIR'] = os.path.join('/',
-                                      os.path.basename(this.get('install')))
-
-    env['PATH'] = ':'.join(path)
+    env['DESTDIR'] = os.path.join('/', os.path.basename(this.get('install')))
     env['PREFIX'] = this.get('prefix') or '/usr'
     env['MAKEFLAGS'] = '-j%s' % (this.get('max-jobs') or
                                  app.config['max-jobs'])
     env['TERM'] = 'dumb'
-    env['SHELL'] = '/bin/sh'
+    env['SHELL'] = '/bin/bash'
     env['USER'] = env['USERNAME'] = env['LOGNAME'] = 'tomjon'
     env['LC_ALL'] = 'C'
     env['HOME'] = '/tmp'
@@ -305,7 +274,7 @@ def env_vars_for_build(defs, this):
     elif arch.startswith('mips64'):
         abi = 'abi64'
     env['TARGET'] = cpu + '-baserock-linux-gnu' + abi
-    env['TARGET_STAGE1'] = cpu + '-bootstrap-linux-gnu' + abi
+    env['TARGET_MUSL'] = cpu + '-bootstrap-linux-musl' + abi
     env['MORPH_ARCH'] = arch
     env['DEFINITIONS_REF'] = app.config['def-version']
     env['PROGRAM_REF'] = app.config['my-version']
@@ -316,21 +285,19 @@ def env_vars_for_build(defs, this):
 
 
 def create_devices(this):
-    perms_mask = stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO
+
+    destdir = os.path.join('/', os.path.basename(this['install'].rstrip(os.path.sep)))
     for device in this['devices']:
-        destfile = os.path.join(this['install'], './' + device['filename'])
-        mode = int(device['permissions'], 8) & perms_mask
-        if device['type'] == 'c':
-            mode = mode | stat.S_IFCHR
-        elif device['type'] == 'b':
-            mode = mode | stat.S_IFBLK
-        else:
-            raise IOError('Cannot create device node %s,'
-                          'unrecognized device type "%s"'
-                          % (destfile, device['type']))
-        app.log(this, "Creating device node", destfile)
-        os.mknod(destfile, mode, os.makedev(device['major'], device['minor']))
-        os.chown(destfile, device['uid'], device['gid'])
+        destfile = os.path.join(destdir, device['filename'].strip(os.path.sep))
+
+        app.log(this, "Creating device node", device['filename'])
+        run_sandboxed(this, 'mknod -m ' + \
+                      device['permissions'] + ' ' + destfile + ' ' + device['type'] + ' ' + \
+                      str(device['major']) + ' ' + \
+                      str(device['minor']) + \
+                      ' && ' +  \
+                      'chown ' + str(device['uid']) + ':' + str(device['gid']) + ' ' + destfile + \
+                      ' || exit 1')
 
 
 def list_files(component):
